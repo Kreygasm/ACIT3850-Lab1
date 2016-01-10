524562c426e49d19267a17a1996e36cca7a1719e
Kreygasm <anderson0phan@gmail.com>
2016-01-10 15:59:21 -0800

    Commenting and naming conventions
    checking over for comments and naming conventions

--- index.php
+++ index.php
@@ -24,15 +24,15 @@ and open the template in the editor.
                     $this->position = str_split($squares);
                 }
                 function __pick_move(){
-                    $array = [];
-                    $openSpace = 0;
-                    for($i=0; $i<9; $i++){
-                        if($this->position[$i] == '-'){
-                            $array[$openSpace] = $i;
-                            $openSpace++;
+                    $array = []; //array of all unselected spots, currently empty.
+                    $open_space = 0; //counter of all unselected spots.
+                    for($i=0; $i<9; $i++){ //loops through the board.
+                        if($this->position[$i] == '-'){ //searching for open spots.
+                            $array[$open_space] = $i; //keeps track of which position an open spot is.
+                            $open_space++;
                         }
                     }
-                    $this->position[$array[round(rand(0, $openSpace-1))]] = 'x';
+                    $this->position[$array[round(rand(0, $open_space-1))]] = 'x'; //make our move.
                 }
                 
                 function __display(){
@@ -48,17 +48,18 @@ and open the template in the editor.
                 function show_cell($which){
                     $token = $this->position[$which];
                     if($token <> '-'){ return '<td>'.$token.'</td>';}
-                    $this-> newposition = $this->position;
-                    $this-> newposition[$which] = 'o';
-                    $move = implode($this->newposition);
+                    $this-> newposition = $this->position; //copy the original.
+                    $this-> newposition[$which] = 'o'; //this is their move.
+                    $move = implode($this->newposition); //create spring from board.
                     
                     $link = 'http://localhost/ACIT4850Lab1/index.php/?board='.$move;
+                    // This is what we want the link to be, return cell with anchor and showing a hyphen.
                     return '<td><a href="'.$link.'">-</a></td>';
                 }
                 
                 function __winner($token){
                     $winner = false;
-                    for($row=0; $row<3; $row++){
+                    for($row=0; $row<3; $row++){ //checking all rows for possible wins.
                         $result = 0;
                         for($col=0; $col<3; $col++){
                             if($this->position[(3*$row + $col)] == $token) {$result++;}
@@ -66,19 +67,18 @@ and open the template in the editor.
                         }
                         if($result == 3){$winner = true;}
                     }
-                    for($col=0; $col<3; $col++){
+                    for($col=0; $col<3; $col++){ //checking all columns for possible wins.
                         $result = 0;
                         for($row=0; $row<3; $row++){
                             if($this->position[($col + 3*$row)] == $token) {$result++;}
                         }
                         if($result == 3){$winner = true;}
                     }
-                    //*diagonal1
+                    //*checking both diagonals for possible wins.
                     if( ($this->position[0] == $token) && 
                         ($this->position[4] == $token) && 
                         ($this->position[8] == $token))
                     { $winner = true; }
-                    //*diagonal2
                     else if(($this->position[2] == $token) && 
                         ($this->position[4] == $token) && 
                         ($this->position[6] == $token))
@@ -88,16 +88,15 @@ and open the template in the editor.
             }
         
         //check if a board parameter was passed
-        if(!isset($_GET['board'])){
-            $position = '---------';
-        }
+        if(!isset($_GET['board'])){$position = '---------'; /*incase a board has not been created yet*/}
         else {$position = $_GET['board'];}
             $temp = "Anderson";
-            echo 'Hi, name is '.$temp.'. ME GO FACE SMOrc SMOrc Kappa';
+            echo 'Hi, name is '.$temp.'. ME GO FACE SMOrc SMOrc Kappa'; // trolling here, don't mind this.
             
-            $game = new Game($position);
-            if ($game->__winner('o') == false ){$game->__pick_move();}
+            $game = new Game($position); //make a new game
+            if ($game->__winner('o') == false ){$game->__pick_move();} //checks for if the player won already.
             $game->__display();
+            // winner checking.
             if($game->__winner('x') == true ){echo 'Me win, get good noob';}
             else if ($game->__winner('o') == true ){echo 'WINNNNNEEERRR';}
             else {echo 'no winner yet';}
